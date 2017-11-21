67de98d2bd95fed15c0cfda137e840997387e16c
salamarxos <pipi_karipi@hotmail.com>
2017-11-21 10:28:48 +0100

    The update position using random walk initially

---
+++ UpdatePositions.m
@@ -0,0 +1,26 @@
+function[positions]=UpdatePositions(numberOfAgents,positions,grid,diffusionRate)    
+tempPositions=positions;
+
+for iAgent=1:numberOfAgents
+    if rand<diffusionRate
+        step=randi(4);
+        if step==1
+            tempPositions(iAgent,1)=positions(iAgent,1)+1;
+        elseif step==2
+            tempPositions(iAgent,2)=positions(iAgent,2)+1;
+        elseif step==3
+            tempPositions(iAgent,1)=positions(iAgent,1)-1;
+        else
+            tempPositions(iAgent,2)=positions(iAgent,2)-1;
+        end
+    end
+end
+    % Apply boundary conditions
+positions = mod(tempPositions,grid);
+end
+
+
+
+
+
+    
