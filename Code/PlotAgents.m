3c139183371b880afa4d531b1872b1e51d165473
BarkenBark <oscar.henrik.bark@gmail.com>
2017-11-21 10:44:45 +0100

    Added clarifying comments to plot functions

--- Code/PlotAgents.m
+++ Code/PlotAgents.m
@@ -1,5 +1,6 @@
 function agentsHandle = PlotAgents(positions, agentsHandle)
-  
+  %First time, call function without second argument to create new handle
+
   if ~exist('agentsHandle', 'var')
     agentsHandle = scatter(positions(:,1), positions(:,2), ...
       'MarkerFaceColor', agentColor);
