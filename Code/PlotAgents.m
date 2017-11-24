1c42501721f18ec38536eafa2c36e9569dfa0727
Kevin Jaquier <kevin.jaquier@gmail.com>
2017-11-24 13:02:18 +0100

    Fixed agents plots reversed

--- Code/PlotAgents.m
+++ Code/PlotAgents.m
@@ -2,10 +2,10 @@ function agentsHandle = PlotAgents(positions, agentColor, agentsHandle)
   %First time, call function without second argument to create new handle
 
   if ~exist('agentsHandle', 'var')
-    agentsHandle = scatter(positions(:,1), positions(:,2), ...
+    agentsHandle = scatter(positions(:,2), positions(:,1), ...
       'MarkerFaceColor', agentColor, 'MarkerEdgeColor', agentColor);
   else
-    set(agentsHandle, 'XData', positions(:,1), 'YData', positions(:,2));
+    set(agentsHandle, 'XData', positions(:,2), 'YData', positions(:,1));
   end
   
 end
