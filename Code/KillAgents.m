c53b8c961fe2f99b34abeab2aaf8fce33ec9dee3
BarkenBark <oscar.henrik.bark@gmail.com>
2017-11-29 10:53:34 +0100

    Forgot to include this file in previous commit

---
+++ Code/KillAgents.m
@@ -0,0 +1,7 @@
+function [positions, inventory, hunger] = KillAgents(positions, inventory, hunger);
+%Simply drop the rows corresponding to the agents whose hungers are above a
+%certain threshold
+
+
+
+end
