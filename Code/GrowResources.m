4e580586e8cc09abcfea76bf415269ffee0b2f31
Kevin Jaquier <kevin.jaquier@gmail.com>
2017-11-30 11:19:15 +0100

    Fixed and optimized GrowResources

--- Code/GrowResources.m
+++ Code/GrowResources.m
@@ -1,12 +1,5 @@
 function grid = GrowResources(grid, resourceGrowthRate)
-    
-    %resourceGrowthRate = 1.25; % resourceGrowthRate \in [1,\infty) 
-                               % (but large values will create crazy values, try to keep under 3)
-    
-    for i=1:size(grid,1)
-       for j = 1:size(grid,2)
-          grid(i,j) = resourceGrowthRate*grid(i,j)*(1-grid(i,j)); 
-       end
-    end
+
+    grid = grid + resourceGrowthRate .* grid .* (1 - grid);
     
 end
