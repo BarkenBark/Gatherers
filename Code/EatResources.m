a4a441be613ea8ddb11e5577ee1b4db2a0236a50
BarkenBark <oscar.henrik.bark@gmail.com>
2017-11-30 17:35:01 +0100

    Changed dynamics to a simpler one: Always eat what you've got to fully sate hunger

--- Code/EatResources.m
+++ Code/EatResources.m
@@ -1,16 +1,8 @@
 function [hunger, inventory] = EatResources(hunger, inventory, hungerRate)
 %The hungrier an agent is, the more it should eat regardless of inventory
 
-  nbrOfAgents = length(hunger);
-
   hunger = hunger + hungerRate;
-  deltaHunger = -hunger.*inventory;
-%   for i = 1:nbrOfAgents
-%     if hunger(i) + deltaHunger(i) < 0 
-%      deltaHunger(i) = -hunger(i);
-%     end
-%   end
-  
+  deltaHunger = -inventory;
 
   tmpHunger = hunger + deltaHunger;
   deltaHunger(tmpHunger < 0) = -hunger(tmpHunger<0);
