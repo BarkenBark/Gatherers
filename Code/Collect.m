19d0ca1b7d9fd075371d302bd3716a4b17c0bdc2
BarkenBark <oscar.henrik.bark@gmail.com>
2017-11-30 11:55:48 +0100

    Collect updated with free parameters

--- Code/Collect.m
+++ Code/Collect.m
@@ -1,20 +1,20 @@
-function [grid, inventory] = Collect(grid, inventory, positions, hunger, collectionCapPerTimeStep)
+function [grid, inventory] = Collect(grid, inventory, positions, hunger, collectionRate)
   %Currently returns the harvested grid, but should also return the
   %collected resources for the agents. 
+  
+nbrOfAgents = size(positions, 1);
 
-    numberOfGatherers = size(positions,1);
-    %numberOfGatherers = length(positions); %better, but not secure for case with only one gatherer?
-    
-    %collectionRate = GetCollectionRate(hunger, inventory); %If we want a more sophisticated way to calculate collectionRate
-    collectionRate = hunger;
-    collection = collectionRate*collectionCapPerTimeStep;
-    
-    for i = 1:numberOfGatherers
-        xPos = positions(i,1);
-        yPos = positions(i,2);
-        grid(xPos,yPos) = 0.1 .* grid(xPos,yPos) .* (1 - grid(xPos,yPos));
-    end
-    
-    inventory = inventory + collection;
+gridLength = size(grid, 1);
+newGrid = zeros(gridLength);
+collection = zeros(nbrOfAgents, 1);
+for i = nbrOfAgents
+  xPos = positions(i,1);
+  yPos = positions(i,2);
+  newGrid(xPos,yPos) = collectionRate.* grid(xPos,yPos) .* (1 - grid(xPos,yPos)); % maximum sustainable yield
+  collection(i) = grid(xPos,yPos) - newGrid(xPos,yPos);
+end
+
+inventory = inventory + collection;
+
     
 end
