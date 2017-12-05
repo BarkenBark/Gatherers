5963c05ef8b83a33c18c90cee5905961feb21424
Kevin Jaquier <kevin.jaquier@gmail.com>
2017-12-05 09:59:46 +0100

    Better agents plot

--- Code/PlotAgents.m
+++ Code/PlotAgents.m
@@ -2,7 +2,8 @@ function agentsHandle = PlotAgents(positions, agentColor, agentsHandle)
   %First time, call function without second argument to create new handle
 
   if ~exist('agentsHandle', 'var')
-    agentsHandle = scatter(positions(:,2), positions(:,1), ...
+    agentsHandle = scatter(positions(:,2), positions(:,1),...
+      75, '.', ...
       'MarkerFaceColor', agentColor, 'MarkerEdgeColor', agentColor);
   else
     set(agentsHandle, 'XData', positions(:,2), 'YData', positions(:,1));
